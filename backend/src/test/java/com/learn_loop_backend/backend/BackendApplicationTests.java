15f3ed880b1e0d7c491819e784b063bdbf446365
WIJERATHNA A.D.S.S <susinidusachinthana@gmail.com>
2025-04-01 11:09:13 +0530

    first commit

---
+++ backend/src/test/java/com/learn_loop_backend/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_loop_backend.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
