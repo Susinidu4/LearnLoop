0056f2013d7691ebf17896f54e15a4dc9f54f613
WIJERATHNA A.D.S.S <susinidusachinthana@gmail.com>
2025-04-06 00:43:13 +0530

    correct header issues

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import Logo from "../assets/images/logo.png";
 
 export const Header = () => {
   return (
-    <header className="fixed z-50 bg-[#2E1A0F] text-white p-2 flex justify-between items-center top-0 w-full shadow-md ">
+    <header className="fixed z-50 bg-[#2E1A0F] text-white p-2 pr-20 flex justify-between items-center top-0 w-full shadow-md ">
       <div > {/* Push logo to right (avoid sidebar overlap) */}
         <img src={Logo} alt="Logo" className="w-70 h-20" />
       </div>
@@ -17,9 +17,9 @@ export const Header = () => {
         <FaBell className="w-6 h-6 cursor-pointer hover:text-[#CFB397] transition duration-300" title="Notification"/>
 
         {/* User Profile */}
-        <div className="flex items-center gap-2 cursor-pointer hover:text-[#CFB397] transition duration-300">
-          <FaCircleUser className="w-10 h-10" title="Profile"/>
-          <MdKeyboardArrowDown className="w-6 h-6" title="Profile"/>
+        <div className="flex items-center gap-6 cursor-pointer hover:text-[#CFB397] transition duration-300">
+          <FaCircleUser size={40} title="Profile"/>
+          <MdKeyboardArrowDown size={24} title="Profile"/>
         </div>
       </div>
 
