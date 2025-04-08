2210326fc7b00dd4cc147ed061f1f887d1afc8ca
Oshini Malshika Chathurangi <malshikaoshini@gmail.com>
2025-04-08 16:13:29 -0700

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
         <Route path="/addpost" element={<AddPost/>}/>
         <Route path="/userviewpost" element={<UserViewPost/>}/>
         <Route path="/homepost" element={<HomePost/>}/>
-        <Route path="/privacypolicy" element={<PrivacyPolicy/>}/>
+        <Route path="/privacypolicy" element={<PrivacyPolicy/>}/>  
         
 
         
