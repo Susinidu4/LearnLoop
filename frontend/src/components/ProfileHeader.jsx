ea27dd3dfd87943db03566747ac2a184dbb2e200
Wijesundara J A I S <155086679+Susinidu4@users.noreply.github.com>
2025-05-16 18:35:38 +0530

    update header

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -3,11 +3,8 @@ import { PencilIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import ProfileService from "../service/Profile & Followers Management/ProfileService";
 import FollowerService from "../service/Profile & Followers Management/FollowService";
-
-import profileBanner from "../assets/images/profileBanner.png"; 
-
+import profileBanner from "../assets/images/profileBanner.png";
 import PostService from "../service/Post-And-Interaction/PostService";
-
 
 export function ProfileHeader() {
   const user = JSON.parse(localStorage.getItem("user"));
@@ -81,7 +78,7 @@ export function ProfileHeader() {
   }
 
   return (
-    <div className="mb-6 py-28">
+    <div className="mb-6 py-8">
       {/* Hidden file input */}
       <input
         type="file"
@@ -100,7 +97,7 @@ export function ProfileHeader() {
         />
         {/* Profile image */}
         <div className="absolute -bottom-28 left-5 flex flex-col items-center">
-          <div className="w-28 h-28 rounded-full border-4 border-[#633D2B] overflow-hidden">
+          <div className="w-40 h-40 rounded-full border-4 border-[#633D2B] overflow-hidden">
             {profileImageUrl ? (
               <img
                 src={profileImageUrl}
@@ -123,9 +120,8 @@ export function ProfileHeader() {
 
         {/* Edit button */}
 
-        <button 
+        <button
           className="absolute bottom-2 right-2 p-2 rounded-full"
-
           onClick={handleEditClick}
         >
           <PencilIcon size={20} color="white" />
@@ -133,7 +129,7 @@ export function ProfileHeader() {
       </div>
 
       {/* Profile stats */}
-      <div className="flex justify-center space-x-8 mb-6">
+      <div className="flex justify-end space-x-8 mb-6 -mt-10">
         <div className="flex flex-col items-center">
           <Link to={`/myfollowers`}>
             <div className="bg-gray-200 rounded-full w-16 h-16 flex items-center justify-center mb-1">
